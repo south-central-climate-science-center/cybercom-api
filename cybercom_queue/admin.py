2a57176fb8343dedca0e1ef27201a7c58c29f191
Mark Stacy <mbstacy@gmail.com>
2016-12-07 11:09:40 -0600

    update admin permission for cybercom_queue

--- cybercom_queue/admin.py
+++ cybercom_queue/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
+# Register your models here.
+from api import config
+from models import taskModel
 
 # Register your models here.
+def setpermissions(app_label,codename,name):
+    try:
+        ct = ContentType.objects.get_for_model(taskModel)
+        Permission.objects.get_or_create(codename=codename, name=name, content_type=ct)
+    except:
+         print("Unable to create {0} permission.".format(codename))
+
+
+#create admin permissions
+setpermissions('cybercom_queue','task_admin',"Task Admin")
